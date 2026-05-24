7079298b340640300dc2c75dc1435ae8fb8ce2e7
Victor Chaves <victor.chaves460@gmail.com>
2026-05-24 20:25:48 -0300

    feat: adicionei a controller para o catalogo

---
+++ src/AgileFood/Controllers/CatalogController.cs
@@ -0,0 +1,26 @@
+using AgileFood.Application.Interfaces.Catalogs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgileFood.Api.Controllers;
+
+[Route("api/catalogo")]
+[ApiController]
+public class CatalogController : ControllerBase
+{
+    private readonly ICatalogService _catalogService;
+
+    public CatalogController(ICatalogService catalogService)
+    {
+        _catalogService = catalogService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAvailable([FromQuery] string? search)
+    {
+        var items = string.IsNullOrWhiteSpace(search)
+            ? await _catalogService.GetAvailableItemsAsync()
+            : await _catalogService.SearchAsync(search);
+
+        return Ok(items);
+    }
+}
